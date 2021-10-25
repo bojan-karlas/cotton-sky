03f024bfa370f0ce320a40ea00fc44e7c046cfb7
Bojan Karlaš <bojan.karlas@gmail.com>
2021-10-25 21:06:33 +0200

    Enable parametrization of TOC header selector.

--- assets/js/scripts/tocbot.js
+++ assets/js/scripts/tocbot.js
@@ -18,13 +18,16 @@ document.addEventListener("DOMContentLoaded", function(event) {
     }
     makeIds();
 
+    let headingSelector = "h2, h3, h4";
+    {{ with $.Site.Params.tocHeadingSelector }} headingSelector = "{{ . }}"{{ end }}
+
     tocbot.init({
         // Where to render the table of contents.
         tocSelector: ".js-toc",
         // Where to grab the headings to build the table of contents.
         contentSelector: "main",
         // Which headings to grab inside of the contentSelector element.
-        headingSelector: "h2, h3, h4",
+        headingSelector: headingSelector,
 
         orderedList: false,
 
