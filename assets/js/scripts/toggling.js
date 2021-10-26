cbb8cdbf261102ca36d78048e1ff2341bbc8df67
Bojan Karlaš <bojan.karlas@gmail.com>
2021-10-26 21:36:40 +0200

    Update menu bar.

--- assets/js/scripts/toggling.js
+++ assets/js/scripts/toggling.js
@@ -12,6 +12,13 @@ function ensureHasClass(query, className) {
     });
 }
 
+function ensureDoesNotHaveClass(query, className) {
+  var elements = document.querySelectorAll(query);
+  Array.prototype.forEach.call(elements, function (element) {
+      element.classList.remove(className);
+  });
+}
+
 function shareOrToggleHidden(query, title, url) {
     if (navigator.share) {
         navigator.share({
@@ -24,10 +31,21 @@ function shareOrToggleHidden(query, title, url) {
       }
 }
 
-var elements = document.querySelectorAll("body>header>nav .selected");
-Array.prototype.forEach.call(elements, function (element) {
-  element.addEventListener("click", function(event) {
-    toggleClass('dropdown');
-    event.preventDefault();
-  }, false);
+document.addEventListener("DOMContentLoaded", function(event) {
+  var elements = document.querySelectorAll("body>header>nav .selected");
+
+  Array.prototype.forEach.call(elements, function (element) {
+    element.addEventListener("click", function(event) {
+      toggleClass("body>header>nav ul", 'dropdown');
+      event.stopPropagation();
+      event.preventDefault();
+    }, false);
+  });
+
+  var elements = document.querySelectorAll("body");
+  Array.prototype.forEach.call(elements, function (element) {
+    element.addEventListener("click", function(event) {
+      ensureDoesNotHaveClass("body>header>nav ul", 'dropdown');
+    }, false);
+  });
 });
