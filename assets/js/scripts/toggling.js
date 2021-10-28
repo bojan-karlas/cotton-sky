cd6ee83c48eb8c9b4f9385ecda03d54491d1eca8
Bojan Karlaš <bojan.karlas@gmail.com>
2021-10-28 14:36:12 +0200

    Fix mobile menu click behavior.

--- assets/js/scripts/toggling.js
+++ assets/js/scripts/toggling.js
@@ -31,21 +31,43 @@ function shareOrToggleHidden(query, title, url) {
       }
 }
 
+
+
 document.addEventListener("DOMContentLoaded", function(event) {
-  var elements = document.querySelectorAll("body>header>nav .selected");
 
-  Array.prototype.forEach.call(elements, function (element) {
-    element.addEventListener("click", function(event) {
-      toggleClass("body>header>nav ul", 'dropdown');
-      event.stopPropagation();
-      event.preventDefault();
-    }, false);
+  var menuUlElement = document.querySelector("body>header>nav ul");
+  var menuLiElements = menuUlElement.querySelectorAll("li");
+
+  var dropDownEventListener = function(event) {
+    toggleClass("body>header>nav ul", 'dropdown-open');
+    event.stopPropagation();
+    event.preventDefault();
+  }
+
+  var swapSelectionListener = function(event) {
+    var targetLi = event.target.closest("li");
+    
+    Array.prototype.forEach.call(menuLiElements, function (element) {
+      if (element === targetLi) {
+        element.classList.add("selected");
+        element.addEventListener("click", dropDownEventListener, false);
+      } else {
+        element.classList.remove("selected");
+        element.removeEventListener("click", dropDownEventListener, false);
+      }
+    });
+  }
+
+  Array.prototype.forEach.call(menuLiElements, function (element) {
+    if (element.classList.contains("selected")) {
+      element.addEventListener("click", dropDownEventListener, false);
+    } else {
+      element.addEventListener("click", swapSelectionListener, false);
+    }
   });
 
-  var elements = document.querySelectorAll("body");
-  Array.prototype.forEach.call(elements, function (element) {
-    element.addEventListener("click", function(event) {
-      ensureDoesNotHaveClass("body>header>nav ul", 'dropdown');
-    }, false);
-  });
+  var bodyElement = document.querySelector("body");
+  bodyElement.addEventListener("click", function(event) {
+    menuUlElement.classList.remove("dropdown-open");
+  }, false);
 });
